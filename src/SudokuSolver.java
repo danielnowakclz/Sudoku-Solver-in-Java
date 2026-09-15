1cc9149c046da5ec741b9cd9850ea6560e37f71d
danielnowakclz <werbung@daniel-nowak.de>
2026-09-15 08:49:37 +0200

    optimizations
    optimizations

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,72 +1,28 @@
-import java.util.Iterator;
-import java.util.Vector;
-
 public class SudokuSolver {
+    public static void main(String[] args) {
+        Sudoku s = new Sudoku();
 
-	private static Vector<Sudoku> sudokuField = new Vector<>();
+        int[] field = {
+            0,0,0, 0,0,0, 0,0,0,
+            0,0,0, 0,0,0, 0,0,0,
+            0,0,0, 0,0,0, 0,0,0,
 
-	public static void main(final String[] args) {
-		final Sudoku sudoku = new Sudoku();
-		
-		final int[] field = { 
-				
-				0,0,0, 0,0,0, 0,0,0,
-				0,0,0, 0,0,0, 0,0,0,
-				0,0,0, 0,0,0, 0,0,0,
-				
-				0,0,0, 0,0,0, 0,0,0,
-				0,0,0, 0,0,0, 0,0,0,
-				0,0,0, 0,0,0, 0,0,0,
-				
-				0,0,0, 0,0,0, 0,0,0,
-				0,0,0, 0,0,0, 0,0,0,
-				0,0,0, 0,0,0, 0,0,0 
-				
-		};
+            0,0,0, 0,0,0, 0,0,0,
+            0,0,0, 0,0,0, 0,0,0,
+            0,0,0, 0,0,0, 0,0,0,
 
-		for (int i = 0; i < field.length; i++)
-			if (field[i] != 0)
-				sudoku.setIN(i, field[i]);
-		sudokuField.add(sudoku);
-		solve();
-	}
+            0,0,0, 0,0,0, 0,0,0,
+            0,0,0, 0,0,0, 0,0,0,
+            0,0,0, 0,0,0, 0,0,0
+        };
 
-	private static void solve() {
-		while (sudokuField.size() > 0) {
-			final Sudoku sudoku = sudokuField.remove(0);
-			int index = sudoku.nextSolvedFieldIndex();
-			while (index > -1) {
-				int y = index / 9;
-				int x = index - 9 * y;
-				y++;
-				x++;
-				final int number = sudoku.sudoku.elementAt(index).possibleNumbers.iterator().next();
-				sudoku.setXYN(x, y, number);
-				index = sudoku.nextSolvedFieldIndex();
-			}
-			if (sudoku.solved()) {
-				sudoku.print();
-				System.exit(0);
-			} else {
-				index = sudoku.nextSplitFieldIndex();
-				if (index > -1) {
-					final Field split = sudoku.sudoku.elementAt(index);
-					final Iterator<Integer> iter = split.possibleNumbers.iterator();
-					while (iter.hasNext()) {
-						final int number = iter.next();
-						final Sudoku element = new Sudoku();
-						element.clone(sudoku);
-						int y = index / 9;
-						int x = index - 9 * y;
-						y++;
-						x++;
-						element.setXYN(x, y, number);
-						sudokuField.add(element);
-					}
-				}
-			}
-		}
-		System.out.println("Not solvable.");
-	}
+        for (int i = 0; i < 81; i++)
+            if (field[i] != 0)
+                s.set(i, field[i]);
 
+        if (s.solve())
+            s.print();
+        else
+            System.out.println("Not solvable.");
+    }
 }
